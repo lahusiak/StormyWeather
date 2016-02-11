25b3b6196f269b0254801c08dbe7af2383d6ed13
lahusiak <lahusiak@gmail.com>
2016-02-11 10:22:35 -0600

    gitignore moved

---
+++ StormyWeather/ForecastKeys.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  StormyWeather
+//
+//  Created by Laryssa Husiak on 2/11/16.
+//  Copyright © 2016 Laryssa Husiak. All rights reserved.
+//
+
+import Foundation
+
+
+
+func valueForAPIKey(keyname:String) -> String {
+       let filePath = NSBundle.mainBundle().pathForResource("ForecastKeys", ofType:"plist")
+            print("This is plistpath:", filePath)
+    
+        let plist = NSDictionary(contentsOfFile:filePath!)
+    
+    
+        let value:String = plist?.objectForKey(keyname) as! String
+        return value
+    }
