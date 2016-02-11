794f8954e77ecfbf09d22b4448f136967ca72ff7
lahusiak <lahusiak@gmail.com>
2016-02-11 10:32:48 -0600

    Add sample to gitignore

---
+++ StormyWeather/Sample.swift
@@ -0,0 +1,11 @@
+//
+//  Sample.swift
+//  StormyWeather
+//
+//  Created by Laryssa Husiak on 2/11/16.
+//  Copyright © 2016 Laryssa Husiak. All rights reserved.
+//
+
+import Foundation
+
+//protect information 
